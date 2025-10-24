b1fedde0a37f1f63543e86946543921024adad74
nurdiyanto29 <55267854+nurdiyanto29@users.noreply.github.com>
2025-10-24 21:01:50 +0700

    Implement AuthService for authentication operations

---
+++ src/app/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private apiUrl = 'http://demo.jejakpustaka.com/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(data: any) {
+    return this.http.post(`${this.apiUrl}/login`, data);
+  }
+
+  logout() {
+    return this.http.post(`${this.apiUrl}/logout`, {});
+  }
+}
